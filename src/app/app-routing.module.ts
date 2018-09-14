d7f560903ea00ffe2832b893bd965fbe300ea9ae
sollahmed <37070490+sollahmed@users.noreply.github.com>
2018-09-14 12:45:38 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {AdminComponent} from './admin/admin.component';
 const routes : Routes = [
-  {path : 'login' , component : LoginComponent}
+  {path : 'login' , component : LoginComponent},
+  {path : 'home' , component : HomeComponent},
+  {path : 'admin' , component : AdminComponent}
 ];
 
 
 @NgModule({
+  imports :[RouterModule.forRoot(routes)],
   exports: [ RouterModule ]
 
 })
